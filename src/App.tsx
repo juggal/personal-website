0670aa9da3eb58ab527f8a308b8c3234fe512ae0
nihar <dasnihar48@gmail.com>
2021-05-25 02:33:36 +0530

    added navbar to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import Home from "./Views/Home";
+import Home from "./Views/Home/Home";
+import { theme } from "./utils";
+import { createMuiTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
 
 function App(): JSX.Element {
-  return <Home />;
+  const muiTheme = createMuiTheme(theme);
+  return (
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/Views/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React, { Component, ReactNode } from "react";
+import { Navbar } from "../../Components/Navbar";
+
+export default class Home extends Component {
+  render(): ReactNode {
+    return <Navbar />;
+  }
+}
