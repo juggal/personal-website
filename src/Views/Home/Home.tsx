7b6600537bfccedfd6c11049b2ee80ae2fa4ba97
nihar <dasnihar48@gmail.com>
2021-05-26 14:10:21 +0530

    svgbutton tested

--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -1,7 +1,13 @@
 import React, { Component, ReactNode } from "react";
+import { SvgButton } from "../../Components/SvgButton";
+import { ReactComponent as email } from "../../assets/svg/email.svg";
 
 export default class Home extends Component {
   render(): ReactNode {
-    return <div>home</div>;
+    return (
+      <div>
+        <SvgButton svg={email} />
+      </div>
+    );
   }
 }
