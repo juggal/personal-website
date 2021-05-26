fbb997a13f583dbe3542a165ff1fbe111632bf44
nihar <dasnihar48@gmail.com>
2021-05-26 16:13:04 +0530

    created service card component

---
+++ src/Components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,42 @@
+import React, { Component, ReactNode } from "react";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  SvgIcon,
+  withStyles,
+  WithStyles,
+} from "@material-ui/core";
+import { styles } from "./styles";
+
+interface ServiceCardProps extends WithStyles<typeof styles> {
+  svg: any;
+  title: string;
+  description: string;
+}
+
+export const ServiceCard = withStyles(styles)(
+  class extends Component<ServiceCardProps> {
+    render(): ReactNode {
+      const { classes, svg, title, description } = this.props;
+      return (
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia className={classes.media}>
+              <SvgIcon
+                className={classes.icon}
+                component={svg}
+                viewBox="0 0 64 64"
+              />
+            </CardMedia>
+            <CardContent className={classes.content}>
+              <span>{title}</span>
+              <p>{description}</p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      );
+    }
+  }
+);
---
+++ src/Components/ServiceCard/styles.tsx
@@ -0,0 +1,29 @@
+import { createStyles, Theme } from "@material-ui/core";
+
+export const styles = ({ palette, typography, spacing, shadows }: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: "464px",
+      maxHeight: "319px",
+      background: palette.secondary.main,
+      borderRadius: "5px",
+      boxShadow: shadows[5],
+    },
+    media: {
+      padding: spacing(2),
+    },
+    icon: {
+      width: spacing(8),
+      height: spacing(8),
+    },
+    content: {
+      color: palette.text.primary,
+      fontSize: spacing(3),
+      "& span": {
+        fontWeight: typography.fontWeightBold,
+      },
+      "& p": {
+        fontWeight: typography.fontWeightRegular,
+      },
+    },
+  });
