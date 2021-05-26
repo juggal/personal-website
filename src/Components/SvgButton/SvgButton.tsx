b7f73c29c7160c62beb3378ffb64691bac1e7505
nihar <dasnihar48@gmail.com>
2021-05-26 19:23:10 +0530

    added classname attribute

--- src/Components/SvgButton/SvgButton.tsx
+++ src/Components/SvgButton/SvgButton.tsx
@@ -1,18 +1,19 @@
-import React, { Component, ReactNode } from "react";
+import React, { Component, HTMLAttributes, ReactNode } from "react";
 import { withStyles, WithStyles, SvgIcon, IconButton } from "@material-ui/core";
 import { styles } from "./styles";
 
 interface SvgButtonProps extends WithStyles<typeof styles> {
   svg: any;
   onClick?: () => void;
+  className?: string;
 }
 
 export const SvgButton = withStyles(styles)(
   class extends Component<SvgButtonProps> {
     render(): ReactNode {
-      const { classes, svg, onClick } = this.props;
+      const { classes, svg, onClick, className } = this.props;
       return (
-        <IconButton onClick={onClick} size="small">
+        <IconButton onClick={onClick} size="small" className={className}>
           <SvgIcon
             className={classes.root}
             component={svg}
