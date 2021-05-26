bdc75b2d5f102017659249704ff9e3ba27af11f8
nihar <dasnihar48@gmail.com>
2021-05-27 00:27:49 +0530

    mock data added

---
+++ src/mock/skills.tsx
@@ -0,0 +1,34 @@
+import { skillType } from "@ctypes";
+
+export const skills: skillType[] = [
+  {
+    id: 1,
+    label: "React JS",
+    value: 60,
+  },
+  {
+    id: 2,
+    label: "Node JS",
+    value: 70,
+  },
+  {
+    id: 3,
+    label: "Python",
+    value: 70,
+  },
+  {
+    id: 4,
+    label: "Flutter",
+    value: 40,
+  },
+  {
+    id: 5,
+    label: "Data Science",
+    value: 20,
+  },
+  {
+    id: 6,
+    label: "Deep Learning",
+    value: 20,
+  },
+];
