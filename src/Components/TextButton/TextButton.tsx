9a27aa1dfaf4663b7a6c9875e01f8afb6443b6a7
nihar <dasnihar48@gmail.com>
2021-05-27 16:42:13 +0530

    bug fix of submit or  download button

--- src/Components/TextButton/TextButton.tsx
+++ src/Components/TextButton/TextButton.tsx
@@ -1,21 +1,36 @@
-import { withStyles, WithStyles } from "@material-ui/core";
+import { Button, withStyles, WithStyles } from "@material-ui/core";
 import React, { Component, ReactNode } from "react";
 import { styles } from "./styles";
 
 interface TextButtonProps extends WithStyles<typeof styles> {
   text: string;
   onClick?: () => void;
+  type: "submit" | "download";
+  link?: string;
 }
 
 export const TextButton = withStyles(styles)(
   class extends Component<TextButtonProps> {
     render(): ReactNode {
-      const { text, onClick, classes } = this.props;
-      return (
-        <button onClick={onClick} className={classes.button}>
-          <span className={classes.text}>{text}</span>
-        </button>
-      );
+      const { text, onClick, classes, type, link } = this.props;
+      if (type === "submit") {
+        return (
+          <Button onClick={onClick} className={classes.button}>
+            <span className={classes.text}>{text}</span>
+          </Button>
+        );
+      }
+      if (type === "download") {
+        return (
+          <Button
+            href={link ? link : ""}
+            target="_blank"
+            className={classes.button}
+          >
+            <span className={classes.text}>{text}</span>
+          </Button>
+        );
+      }
     }
   }
 );
