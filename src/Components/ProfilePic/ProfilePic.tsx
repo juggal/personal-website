4c665e2265ff2a505882d39831553902035ae304
nihar <dasnihar48@gmail.com>
2021-05-27 16:19:23 +0530

    minor change

--- src/Components/ProfilePic/ProfilePic.tsx
+++ src/Components/ProfilePic/ProfilePic.tsx
@@ -3,15 +3,14 @@ import { withStyles, WithStyles, Avatar } from "@material-ui/core";
 import { styles } from "./styles";
 
 interface ProfilPicProps extends WithStyles<typeof styles> {
-  alt: string;
   src: string;
 }
 
 export const ProfilePic = withStyles(styles)(
   class extends Component<ProfilPicProps> {
     render(): ReactNode {
-      const { classes, alt, src } = this.props;
-      return <Avatar alt={alt} src={src} className={classes.root} />;
+      const { classes, src } = this.props;
+      return <Avatar src={src} className={classes.root} />;
     }
   }
 );
