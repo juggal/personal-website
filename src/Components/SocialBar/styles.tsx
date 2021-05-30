cfd90c29b89caa680bac4328b7493aaf415b94aa
nihar <dasnihar48@gmail.com>
2021-05-31 02:47:02 +0530

    made socialBar responsive

--- src/Components/SocialBar/styles.tsx
+++ src/Components/SocialBar/styles.tsx
@@ -1,11 +1,17 @@
 import { createStyles, Theme } from "@material-ui/core";
 
-export const styles = ({ spacing }: Theme) =>
+export const styles = ({ spacing, breakpoints }: Theme) =>
   createStyles({
     root: {
       display: "inline",
       "& a:not(:last-child)": {
         marginRight: spacing(6),
+      },
+      [breakpoints.only("xs")]: {
+        marginLeft: spacing(1),
+        "& a:not(:last-child)": {
+          marginRight: spacing(4),
+        },
       },
     },
   });
