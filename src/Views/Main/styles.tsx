557ae5e9fb277646ac978b472a944f3ba974d6ca
nihar <dasnihar48@gmail.com>
2021-05-31 17:23:57 +0530

    updated media queries for main view

--- src/Views/Main/styles.tsx
+++ src/Views/Main/styles.tsx
@@ -6,29 +6,36 @@ export const styles = ({ breakpoints }: Theme) =>
       height: "100vh",
     },
     navbar: {
+      [breakpoints.down("sm")]: {
+        display: "none",
+      },
       [breakpoints.up("md")]: {
         display: "block",
         height: "8%",
       },
-      [breakpoints.down("sm")]: {
-        display: "none",
-      },
     },
     hamburger: {
+      [breakpoints.only("xs")]: {
+        display: "block",
+        height: "8%",
+      },
+      [breakpoints.only("sm")]: {
+        display: "block",
+        height: "6%",
+      },
       [breakpoints.up("md")]: {
         display: "none",
-      },
-      [breakpoints.down("sm")]: {
-        display: "block",
-        height: "10%",
       },
     },
     view: {
-      [breakpoints.up("md")]: {
+      [breakpoints.only("xs")]: {
         height: "92%",
       },
-      [breakpoints.down("sm")]: {
-        height: "90%",
+      [breakpoints.only("sm")]: {
+        height: "94%",
+      },
+      [breakpoints.up("md")]: {
+        height: "92%",
       },
     },
   });
