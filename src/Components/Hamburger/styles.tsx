64781133754f1cd8190b9c2e557fa76ffc1c754c
nihar <dasnihar48@gmail.com>
2021-05-31 16:26:26 +0530

    sm height updated

--- src/Components/Hamburger/styles.tsx
+++ src/Components/Hamburger/styles.tsx
@@ -8,7 +8,7 @@ export const styles = ({ palette, zIndex, spacing, breakpoints }: Theme) =>
         height: "8%",
       },
       [breakpoints.only("sm")]: {
-        height: "6%",
+        height: "8%",
       },
       zIndex: zIndex.drawer + 1,
       position: "fixed",
