98fd1ba556240c18fbc6e176aca6550aea947064
nihar <dasnihar48@gmail.com>
2021-06-01 00:34:04 +0530

    divider and services view added to main

--- src/Views/Main/Main.tsx
+++ src/Views/Main/Main.tsx
@@ -1,8 +1,9 @@
 import React, { Component, ReactNode } from "react";
-import { withStyles, WithStyles } from "@material-ui/core";
+import { withStyles, WithStyles, Divider } from "@material-ui/core";
 import { styles } from "./styles";
 import { Navbar } from "@components/Navbar";
 import { Home } from "@views/Home";
+import { Services } from "@views/Services";
 import { Hamburger } from "@components/Hamburger";
 
 interface MainProps extends WithStyles<typeof styles> {}
@@ -21,6 +22,9 @@ export const Main = withStyles(styles)(
           </div>
           <div className={classes.view}>
             <Home />
+            <Divider variant="middle" light />
+            <Services />
+            <Divider variant="middle" light />
           </div>
         </div>
       );
