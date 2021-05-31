3d1afc01b4130e148f021609328e2e6bcb698a9d
nihar <dasnihar48@gmail.com>
2021-05-31 17:22:57 +0530

    flexible width and height added

--- src/Components/ServiceCard/styles.tsx
+++ src/Components/ServiceCard/styles.tsx
@@ -3,8 +3,8 @@ import { createStyles, Theme } from "@material-ui/core";
 export const styles = ({ palette, typography, spacing, shadows }: Theme) =>
   createStyles({
     root: {
-      maxWidth: spacing(58),
-      maxHeight: spacing(40),
+      height: "100%",
+      width: "100%",
       background: palette.secondary.main,
       borderRadius: "5px",
       boxShadow: shadows[5],
