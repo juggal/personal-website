1c05f635c8ec25229abd1132584968862b6565f3
nihar <dasnihar48@gmail.com>
2021-06-10 13:25:40 +0530

    added responsiveness to footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ReactNode } from "react";
-import { withStyles, WithStyles } from "@material-ui/core";
+import { Grid, Typography, withStyles, WithStyles } from "@material-ui/core";
 import { styles } from "./styles";
 
 interface FooterProps extends WithStyles<typeof styles> {
@@ -10,7 +10,18 @@ export const Footer = withStyles(styles)(
   class extends Component<FooterProps> {
     render(): ReactNode {
       const { text, classes } = this.props;
-      return <footer className={classes.root}>{text}</footer>;
+      return (
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          className={classes.root}
+        >
+          <Grid item>
+            <Typography variant="h6">{text}</Typography>
+          </Grid>
+        </Grid>
+      );
     }
   }
 );
--- src/Components/Footer/styles.tsx
+++ src/Components/Footer/styles.tsx
@@ -1,18 +1,26 @@
 import { createStyles, Theme } from "@material-ui/core";
 
-export const styles = ({ palette, typography, spacing }: Theme) =>
+export const styles = ({ palette, typography, breakpoints, spacing }: Theme) =>
   createStyles({
     root: {
-      position: "absolute",
-      bottom: "0px",
       width: "100%",
-      height: spacing(6),
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
       backgroundColor: palette.secondary.main,
       color: palette.text.primary,
-      fontWeight: typography.fontWeightMedium,
-      fontSize: spacing(2),
+      [breakpoints.up("xl")]: {
+        height: "6%",
+      },
+      [breakpoints.only("lg")]: {
+        height: "8%",
+      },
+      [breakpoints.only("md")]: {
+        height: "6%",
+      },
+      [breakpoints.only("sm")]: {
+        height: "5%",
+      },
+      [breakpoints.only("xs")]: {
+        height: "6%",
+        marginTop: spacing(10),
+      },
     },
   });
